a94bc516245184e593e291c95208f4db46d7d7b6
rainDelay9 <matan.orland@gmail.com>
2021-07-01 22:23:13 +0300

    add cli + hidden files toggle

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,40 @@ use core::panic;
 use std::fs;
 use std::fs::metadata;
 use std::path::PathBuf;
+use structopt::StructOpt;
 use termion::{color, style};
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "tree", about = "show dir tree")]
+struct Opt {
+    /// Input path
+    #[structopt(parse(from_os_str), short = "p", long = "path", default_value = ".")]
+    path: PathBuf,
+
+    /// Show hidden files
+    #[structopt(long)]
+    hidden: bool,
+}
+
 fn main() {
-    let mut arguments = std::env::args().skip(1);
-    let mut path = PathBuf::new();
-    let local = arguments.next().unwrap_or(".".to_owned());
-    path.push(&local);
+    let args = Opt::from_args();
+    let path = args.path;
     let md = metadata(&path).expect("non-existent path!");
     if !md.is_dir() {
         panic!("input path is not a directory!");
     }
-    println!("{}", to_colored(local));
-    print_path(path);
+    println!(
+        "{}",
+        to_colored(path.to_str().expect("internal error!").to_owned())
+    );
+    print_path(path, args.hidden);
 }
 
-fn print_path(path: PathBuf) {
-    print_path_helper(path, String::new());
+fn print_path(path: PathBuf, show_hidden: bool) {
+    print_path_helper(path, show_hidden, String::new());
 }
 
-fn print_path_helper(path: PathBuf, prefix: String) {
+fn print_path_helper(path: PathBuf, show_hidden: bool, prefix: String) {
     if let Ok(entries) = fs::read_dir(path) {
         for entry in entries {
             if let Ok(entry) = entry {
@@ -30,13 +44,13 @@ fn print_path_helper(path: PathBuf, prefix: String) {
                         Ok(s) => s,
                         Err(_) => return,
                     };
-                    if file_name.starts_with(".") {
+                    if !show_hidden && file_name.starts_with(".") {
                         continue;
                     }
                     println!("{}", format_path(file_name, prefix.clone(), md.is_file()));
                     if md.is_dir() {
-                        let new_prefix = prefix.clone() + " ";
-                        print_path_helper(entry.path(), new_prefix);
+                        let new_prefix = prefix.clone() + "  ";
+                        print_path_helper(entry.path(), show_hidden, new_prefix);
                     }
                 }
             }
